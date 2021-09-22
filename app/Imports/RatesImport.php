b78d7b1d8dec3f15ee78297e4625432adebaf2e2
Nahuel Savedra <nsavedra97@gmail.com>
2021-09-22 18:50:16 -0300

    method for import excel in model

--- app/Imports/RatesImport.php
+++ app/Imports/RatesImport.php
@@ -2,11 +2,20 @@
 
 namespace App\Imports;
 
+use App\Models\Contract;
 use App\Models\Rates;
 use Maatwebsite\Excel\Concerns\ToModel;
 
 class RatesImport implements ToModel
 {
+    private $contract;
+
+    public function __construct()
+    {
+        $this->contract = Contract::latest()->first();
+    }
+
+
     /**
     * @param array $row
     *
@@ -15,7 +24,14 @@ class RatesImport implements ToModel
     public function model(array $row)
     {
         return new Rates([
-            //
+            'origin' => $row[0],
+            'destination' => $row[1],
+            'currency' => $row[4],
+            'twenty' => $row[5],
+            'forty' => $row[6],
+            'fortyhc' => $row[7],
+            'contract_id' => $this->contract->id,
+
         ]);
     }
 }
