d505acfcbf5353dfcc19d17d5b1492b174b98aba
Nahuel Savedra <nsavedra97@gmail.com>
2021-09-22 18:51:10 -0300

    Added form

---
+++ resources/views/rates/import.blade.php
@@ -0,0 +1,9 @@
+
+
+    <form action="{{ route('rates.store') }}" method='POST' enctype="multipart/form-data">
+        @csrf
+        <input type="text" placeholder="Nombre" name="nombre"/>
+        <input type="date" placeholder="fecha" name="fecha"/>
+        <input name="file" type="file"/>
+        <input type="submit" value="Enviar"/>
+    </form>
