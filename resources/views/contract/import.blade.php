97db8669352a96aeaec906de369bf1704f811819
Nahuel Savedra <nsavedra97@gmail.com>
2021-09-23 13:19:09 -0300

    Added validation in view

--- resources/views/contract/import.blade.php
+++ resources/views/contract/import.blade.php
@@ -3,7 +3,17 @@
     <form action="{{ route('contract.store') }}" method='POST' enctype="multipart/form-data">
         @csrf
         <input type="text" placeholder="Nombre" name="nombre"/>
+        @error('nombre')
+        <p id="nombre">{{$message}}</p>
+        @enderror
         <input type="date" placeholder="fecha" name="fecha"/>
+        @error('fecha')
+        <p id="fecha">{{$message}}</p>
+        @enderror
         <input name="file" type="file"/>
+        @error('file')
+        <p id="files">{{$message}}</p>
+        @enderror
         <input type="submit" value="Enviar"/>
+
     </form>
